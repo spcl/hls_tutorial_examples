a0e6c5630c6aa71561f6c08b87c7142d299c4fad
Johannes de Fine Licht <johannes@musicmedia.dk>
2018-11-05 14:26:31 +0100

    Updated names for example 0

--- example_0/Synthesis.tcl
+++ example_0/Synthesis.tcl
@@ -1,7 +1,7 @@
 open_project hls 
 open_solution solution 
 set_part xcvu9p-flgb2104-2-i 
-add_files -cflags "-DHLSLIB_SYNTHESIS -std=c++11 -I../hlslib/include" "example0.cpp entry.cpp" 
+add_files -cflags "-DHLSLIB_SYNTHESIS -std=c++11 -I../hlslib/include" "Example0.cpp Entry.cpp" 
 set_top Entry 
 create_clock -period 300MHz -name default
 csynth_design
