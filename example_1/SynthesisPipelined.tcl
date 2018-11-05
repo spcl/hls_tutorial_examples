0e4b6b39bf2bc1219ef0f0dc256251017631db7d
Johannes de Fine Licht <johannes@musicmedia.dk>
2018-11-05 14:25:06 +0100

    Updated naming for example 1

---
+++ example_1/SynthesisPipelined.tcl
@@ -0,0 +1,8 @@
+open_project hls_pipelined 
+open_solution example1 
+set_part xcvu9p-flgb2104-2-i 
+add_files -cflags "-DHLSLIB_SYNTHESIS -std=c++11 -I../hlslib/include" "Example1_Pipelined.cpp Entry.cpp" 
+set_top Entry 
+create_clock -period 300MHz -name default
+csynth_design
+exit
--- example_1/SynthesisSolution.tcl
+++
@@ -1,8 +0,0 @@
-open_project hls_solution 
-open_solution example1 
-set_part xcvu9p-flgb2104-2-i 
-add_files -cflags "-DHLSLIB_SYNTHESIS -std=c++11 -I../hlslib/include" "example1_solution.cpp entry.cpp" 
-set_top Entry 
-create_clock -period 300MHz -name default
-csynth_design
-exit
