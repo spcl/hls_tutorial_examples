a18cb3d2daf49973080f75e2947d77f32d75b98a
Johannes de Fine Licht <johannes@musicmedia.dk>
2019-11-17 14:30:59 +0100

    Don't use streams in example 3

--- example_3/xilinx/Example3.cpp
+++ example_3/xilinx/Example3.cpp
@@ -2,36 +2,29 @@
 
 void Stencil2D(float const memory_in[N * M], float memory_out[N * M]) {
 
-  Stream<float> above_buffer(M);
-  Stream<float> center_buffer(M);
+  float above_buffer[M];
+  float center_buffer[M];
 
   for (int i = 0; i < M; ++i) {
     #pragma HLS PIPELINE
-    above_buffer.Push(memory_in[i]);
+    above_buffer[i] = memory_in[i];
   }
 
   for (int i = 0; i < M; ++i) {
     #pragma HLS PIPELINE
-    center_buffer.Push(memory_in[M + i]);
+    center_buffer[i] = memory_in[M + i];
   }
 
   for (int i = 1; i < N - 1; ++i) {
     for (int j = 0; j < M; ++j) {
       #pragma HLS PIPELINE II=1
 
-      const auto above = above_buffer.Pop();
-      const auto center = center_buffer.Pop();
+      const auto above = above_buffer[j];
+      const auto center = center_buffer[j];
       const auto below = memory_in[(i + 1) * M + j];
 
       constexpr float factor = 0.3333;
       const auto average = factor * (above + center + below);
-
-      // Avoid leftover elements in the buffer by not pushing on the last
-      // iteration
-      if (i < N - 2) {
-        above_buffer.Push(center);
-        center_buffer.Push(below);
-      }
 
       memory_out[i * M + j] = average;
     }
